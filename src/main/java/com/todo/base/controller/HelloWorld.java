63df425e07c8684bb1ff42047800a0ade9fdad5a
Yusuke-Inatomi <shonanlove19980704@gmail.com>
2020-10-23 20:12:36 +0900

    first commit

---
+++ src/main/java/com/todo/base/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.todo.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorld {
+	@GetMapping({ "/" })
+	public String hello() {
+		return "/hello";
+	}
+}
